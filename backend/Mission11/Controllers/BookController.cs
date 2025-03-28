07237179658618c253288cd0db40eea3453ff5f3
shicr <shicr@byu.edu>
2025-03-28 17:04:58 -0600

    Adding untracked backend files

---
+++ backend/Mission11/Controllers/BookController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mission11.Data;
+
+namespace Mission11.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+
+        private BookDbContext _bookContext;
+
+
+        public BookController(BookDbContext temp) => _bookContext = temp;
+
+
+        [HttpGet("AllBooks")]
+        public IActionResult GetBooks(int pageHowMany = 5, int pageNum = 1)
+        {
+            var something = _bookContext.Books
+                .Skip((pageNum-1)* pageHowMany)
+                .Take(pageHowMany)
+                .ToList();
+
+            var totalNumProjects = _bookContext.Books.Count();
+
+            var someObject = new
+            {
+                Books = something,
+                TotalNumBooks = totalNumProjects
+            };
+
+            return Ok(someObject);
+        }
+
+        [HttpGet("FunctionalBooks")]
+        public IEnumerable<Book> GetFunctionalBooks()
+        {
+            var something = _bookContext.Books.Where(p => p.Category == "Functional").ToList();
+            return something;
+        }
+    }
+
+}
+
